1251f3d06295a1e27d7e97dafa1fc9c812f13d27
Chroq <christophe.lecroq@gmail.com>
2019-11-28 14:43:56 +0100

    Add test on isStoneInBounds

--- tests/Hex/GameTest.php
+++ tests/Hex/GameTest.php
@@ -17,48 +17,14 @@ class GameTest extends TestCase
 
     /**
      * @test
-     * @covers ::addStone
-     */
-    public function a_user_can_add_a_stone()
-    {
-        $game = new Game(5);
-
-        $game->addStone(0, 0);
-        $this->assertContains([0, 0], $game->getStones());
-
-        $game->addStone(4, 4);
-        $this->assertContains([4, 4], $game->getStones());
-    }
-
-    /**
-     * @test
-     * @covers ::addStone
-     */
-    public function a_user_cant_add_a_stone_in_already_used_place()
-    {
-        $game = new Game(5);
-
-        $game->addStone(0, 0);
-        $this->assertContains([0, 0], $game->getStones());
-
-        $game->addStone(0, 0);
-        $this->assertEquals(1, count($game->getStones()));
-    }
-
-    /**
-     * @test
-     * @covers ::addStone
+     * @covers ::isStoneInBounds
      */
     public function a_user_cant_add_a_stone_outside_the_board()
     {
-        $game = new Game(5);
+        $game = new Game(Game::BOARD_MINI_SIZE);
 
-        $game->addStone(-1, 10);
-
-        $this->assertNotContains([-1, 10], $game->getStones());
-
-        $game->addStone(10, -1);
-        $this->assertNotContains([10, -1], $game->getStones());
+        $this->assertFalse($game->isStoneInBounds(-1, 2));
+        $this->assertFalse($game->isStoneInBounds(10, 1));
     }
 
 
